720f9d131014377da5fa41ac96c7b7dfae6dba05
Binh Vu <binh@toan2.com>
2018-10-07 21:40:06 -0700

    refactor

--- pyready/postgres.py
+++ pyready/postgres.py
@@ -1,34 +1,39 @@
-import sys
-from typing import *
+import psycopg2
+
+from pyready.base import TestConnectionResult, wait_for_connection, get_argparser
+from pyready.telnet import test_tcp_connection
+
+
+def test_postgres_connection(db_host: str, db_port: int, db_name: str, user: str, pwd: str, n_retries: int, verbose: bool=True):
+    def test_func():
+        try:
+            conn = psycopg2.connect(host=db_host, port=db_port, dbname=db_name, user=user, password=pwd, connect_timeout=2)
+            conn.close()
+        except psycopg2.OperationalError as ex:
+            if str(ex).find('timeout expired') != -1:
+                return TestConnectionResult.timeout()
+            return TestConnectionResult.error("Connection failed with error: {0}".format(ex))
+
+        return TestConnectionResult.success()
+
+    if not test_tcp_connection(db_host, db_port, n_retries, verbose):
+        return False
+
+    return wait_for_connection(test_func, f"Test Postgres Connection at {db_host}:{db_port}/{db_name}", n_retries, verbose)
 
 
 if __name__ == '__main__':
-    import psycopg2
+    parser = get_argparser(description="")
+    parser.add_argument("-r", "--retries", type=int, default=10, help="number of retries (default 10)")
+    parser.add_argument("-v", "--verbose", type="boolean", default=True,
+                        help="enable/disable printing messages (default true)")
 
-    conn = sys.argv[1]
-    n_retries = int(sys.argv[2])
+    parser.add_argument("-u", "--user", type=str, help="user name")
+    parser.add_argument("-p", "--password", type=str, help="password")
+    parser.add_argument("host", type=str, help="host name or ip address")
+    parser.add_argument("port", type=int, help="port number")
+    parser.add_argument("db_name", type=str, help="Database name")
 
-    # conn = "host=localhost port=5433 dbname=dcat user=dcat_user password=d393c705ea42"
-    # n_retries = 10
-    success = False
-    print(f"Trying to connect to: `{conn}`: ", end="", flush=True)
-
-    for _ in range(n_retries):
-        try:
-            conn = psycopg2.connect(conn, connect_timeout=2)
-            conn.close()
-            success = True
-            break
-        except psycopg2.OperationalError as ex:
-            if str(ex).find('timeout expired') != -1:
-                print(".", end="", flush=True)
-            else:
-                print("Connection failed: {0}".format(ex))
-                exit(-1)
-
-    if success:
-        print("\nConnect successfully!")
-        exit(0)
-    else:
-        print("\nCannot connect to Postgres")
-        exit(-1)
+    args = parser.parse_args()
+    resp = test_postgres_connection(args.host, args.port, args.db_name, args.user, args.password, args.retries, args.verbose)
+    exit(0 if resp else 1)
